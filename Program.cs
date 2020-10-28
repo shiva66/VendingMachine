9c8e909a882f56f4a237b1520d6495d484f849b3
Pontus Håkansson <phhakansson@gmail.com>
2020-10-28 10:14:10 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VendingMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        class Money
+        {
+            List<int> moneyList = new List<int>();
+        }
+    }
+}
